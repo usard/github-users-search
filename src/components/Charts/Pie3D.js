790c0ebf361270754dbad0de2594b30f5289621d
usard <umasai.anvesh@gmail.com>
2023-09-29 19:12:35 +0530

    created pie chart for languages and kept it in repos component

--- src/components/Charts/Pie3D.js
+++ src/components/Charts/Pie3D.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import FusionCharts from "fusioncharts";
+import Charts from "fusioncharts/fusioncharts.charts";
+import ReactFC from "react-fusioncharts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 
-const Pie3D = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+
+const Pie3D = ({ chartData }) => {
+  const chartConfigs = {
+    type: "pie3d", // The chart type
+    width: "500", // Width of the chart
+    height: "500", // Height of the chart
+    dataFormat: "json", // Data type
+    dataSource: {
+      // Chart Configuration
+      chart: {
+        caption: "languages", //Set the chart caption
+        // subCaption: "xxxxxxxxxxxxxxxxxx", //Set the chart subcaption
+        theme: "fusion", //Set the theme for your chart
+      },
+      data: chartData,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
 export default Pie3D;
--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React from "react";
+import styled from "styled-components";
+import { useGithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { userRepos } = useGithubContext();
+  console.log("repos :", userRepos);
+  const languages = userRepos.reduce((total, item) => {
+    const { language } = item;
+    if (!language) {
+      return total;
+    }
+    if (total[language] === undefined) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+      };
+    }
+    return total;
+  }, {});
+
+  console.log(languages);
+  const chartData = Object.values(languages); // convert object of objects into array of objects
+
+  console.log(chartData);
+  return (
+    <Wrapper className="section-center">
+      <Pie3D chartData={chartData}></Pie3D>;
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.div`
@@ -18,16 +44,16 @@ const Wrapper = styled.div`
     grid-template-columns: 2fr 3fr;
   }
 
-  div {
-    width: 100% !important;
-  }
-  .fusioncharts-container {
-    width: 100% !important;
-  }
-  svg {
-    width: 100% !important;
-    border-radius: var(--radius) !important;
-  }
+  // div {
+  //   width: 100% !important;
+  // }
+  // .fusioncharts-container {
+  //   width: 100% !important;
+  // }
+  // svg {
+  //   width: 100% !important;
+  //   border-radius: var(--radius) !important;
+  // }
 `;
 
 export default Repos;
